cc7d48e21f7e60746c8fd41e0a446a3e7f4303d6
Olly Larkin <olly.larkin@softwire.com>
2020-07-13 13:06:43 +0100

    Part 1 android working

--- SharedCode/src/commonMain/kotlin/ApplicationPresenter.kt
+++ SharedCode/src/commonMain/kotlin/ApplicationPresenter.kt
@@ -16,4 +16,8 @@ class ApplicationPresenter: ApplicationContract.Presenter() {
         this.view = view
         view.setLabel(createApplicationScreenMessage())
     }
+
+    override fun getTimesRequest(departure: String, destination: String): String {
+        return "https://mobile-api-dev.lner.co.uk/v1/fares?originStation=${departure}&destinationStation=${destination}&noChanges=false&numberOfAdults=2&numberOfChildren=0&journeyType=single&inboundDateTime=2020-07-01T12:16:27.371&inboundIsArriveBy=false&outboundDateTime=2020-07-14T19%3A30%3A00.000%2B01%3A00&outboundIsArriveBy=false"
+    }
 }
--- app/src/main/java/com/jetbrains/handson/mpp/mobile/MainActivity.kt
+++ app/src/main/java/com/jetbrains/handson/mpp/mobile/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.jetbrains.handson.mpp.mobile
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity(), ApplicationContract.View {
 
@@ -12,9 +18,46 @@ class MainActivity : AppCompatActivity(), ApplicationContract.View {
 
         val presenter = ApplicationPresenter()
         presenter.onViewTaken(this)
+
+        val departureSpinner: Spinner = findViewById(R.id.departure_station_spinner)
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.stations_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            departureSpinner.adapter = adapter
+        }
+
+        val destinationSpinner: Spinner = findViewById(R.id.destination_station_spinner)
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.stations_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            destinationSpinner.adapter = adapter
+        }
     }
 
     override fun setLabel(text: String) {
         findViewById<TextView>(R.id.main_text).text = text
+    }
+
+    fun getJourneyButtonClick(view: View) {
+        val departureSpinner = findViewById<Spinner>(R.id.departure_station_spinner)
+        val destinationSpinner = findViewById<Spinner>(R.id.destination_station_spinner)
+        val presenter = ApplicationPresenter()
+        val request = presenter.getTimesRequest(departureSpinner.selectedItem.toString(), destinationSpinner.selectedItem.toString())
+        val intent = Uri.parse(request).let { webpage ->
+            Intent(Intent.ACTION_VIEW, webpage)
+        }
+        startActivity(intent)
     }
 }
