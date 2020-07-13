04f10734e8fb67d48ccc30f29ca52d15efd55f4c
Elliot Barnes <Elliotbarnes123@gmail.com>
2020-07-13 17:24:42 +0100

    IOS button launches website

--- SharedCode/src/commonMain/kotlin/ApplicationContract.kt
+++ SharedCode/src/commonMain/kotlin/ApplicationContract.kt
@@ -5,9 +5,11 @@ import kotlinx.coroutines.CoroutineScope
 interface ApplicationContract {
     interface View {
         fun setLabel(text: String)
+        fun showAlert(text: String)
     }
 
     abstract class Presenter: CoroutineScope {
         abstract fun onViewTaken(view: View)
+        abstract fun onButtonPressed(origin: String, destination: String): String
     }
 }
--- SharedCode/src/commonMain/kotlin/ApplicationPresenter.kt
+++ SharedCode/src/commonMain/kotlin/ApplicationPresenter.kt
@@ -8,7 +8,11 @@ class ApplicationPresenter: ApplicationContract.Presenter() {
     private val dispatchers = AppDispatchersImpl()
     private var view: ApplicationContract.View? = null
     private val job: Job = SupervisorJob()
-
+    private val codeMap = mapOf<String, String>("Harrow and Wealdstone" to  "HRW",
+        "Canley" to "CNL",
+        "London Euston" to "EUS",
+        "Coventry" to "COV",
+        "Birmingham New Street" to "BHM")
     override val coroutineContext: CoroutineContext
         get() = dispatchers.main + job
 
@@ -16,4 +20,17 @@ class ApplicationPresenter: ApplicationContract.Presenter() {
         this.view = view
         view.setLabel(createApplicationScreenMessage())
     }
+
+    override fun onButtonPressed(origin: String, destination: String): String {
+        val originCode = codeMap[origin]
+        val destinationCode = codeMap[destination]
+        return if (originCode != null && destinationCode != null) {
+            "https://www.lner.co.uk/travel-information/travelling-now/live-train-times/depart/$originCode/$destinationCode/#LiveDepResults"
+        } else {
+            "https://www.lner.co.uk/travel-information/travelling-now/live-train-times/depart/EUS/BHM/#LiveDepResults"
+        }
+    }
+
+
+
 }
