988c8c2f52249e0337f5d260692d6bcbafdc1f5d
Thomas Haslam <thomas.haslam@softwire.com>
2020-07-13 11:37:45 +0100

    spinners

--- app/src/main/java/com/jetbrains/handson/mpp/mobile/MainActivity.kt
+++ app/src/main/java/com/jetbrains/handson/mpp/mobile/MainActivity.kt
@@ -2,6 +2,8 @@ package com.jetbrains.handson.mpp.mobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity(), ApplicationContract.View {
@@ -12,6 +14,20 @@ class MainActivity : AppCompatActivity(), ApplicationContract.View {
 
         val presenter = ApplicationPresenter()
         presenter.onViewTaken(this)
+        val spinners= listOf<Spinner>(findViewById(R.id.arrival_station),findViewById(R.id.departure_station))
+// Create an ArrayAdapter using the string array and a default spinner layout
+        spinners.forEach {
+            ArrayAdapter.createFromResource(
+                this,
+                R.array.stations_array,
+                android.R.layout.simple_spinner_item
+            ).also { adapter ->
+                // Specify the layout to use when the list of choices appears
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                // Apply the adapter to the spinner
+                it.adapter = adapter
+            }
+        }
     }
 
     override fun setLabel(text: String) {
