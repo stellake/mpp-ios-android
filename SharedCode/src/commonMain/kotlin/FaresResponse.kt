84349bf28669ca38c34c5501de3046934d13c93d
Priyanka Patel <priyanka.patel@softwire.com>
2020-07-14 17:18:27 +0100

    deserialization of json requests

---
+++ SharedCode/src/commonMain/kotlin/FaresResponse.kt
@@ -0,0 +1,27 @@
+package com.jetbrains.handson.mpp.mobile
+
+import kotlinx.serialization.*
+import kotlinx.serialization.json.*
+
+@Serializable
+data class FaresResponse (
+    val outboundJourneys: List<JourneyOption>
+)
+
+@Serializable
+data class JourneyOption (
+    val journeyOptionToken: String,
+    val journeyId: String,
+    val originStation: Station,
+    val destinationStation: Station,
+    val departureTime: String,
+    val arrivalTime: String,
+    val journeyDurationInMinutes: Int
+    )
+
+@Serializable
+data class Station (
+    val displayName: String,
+    val crs: String,
+    val nlc: String
+)
