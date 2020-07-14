57d06aa245d1a7e0a90ca9561be01c002ff3828d
Elliot Barnes <Elliotbarnes123@gmail.com>
2020-07-14 14:20:25 +0100

    Makes an API Call

--- SharedCode/src/commonMain/kotlin/ApplicationPresenter.kt
+++ SharedCode/src/commonMain/kotlin/ApplicationPresenter.kt
@@ -1,6 +1,14 @@
 package com.jetbrains.handson.mpp.mobile
 
+import io.ktor.client.HttpClient
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.json.serializer.KotlinxSerializer
+import io.ktor.client.request.get
+import io.ktor.client.statement.HttpResponse
+import io.ktor.client.statement.readBytes
+import io.ktor.client.statement.readText
 import kotlinx.coroutines.*
+import kotlinx.serialization.serializer
 import kotlin.coroutines.CoroutineContext
 
 class ApplicationPresenter: ApplicationContract.Presenter() {
@@ -24,13 +32,31 @@ class ApplicationPresenter: ApplicationContract.Presenter() {
     override fun onButtonPressed(origin: String, destination: String): String {
         val originCode = codeMap[origin]
         val destinationCode = codeMap[destination]
-        return if (originCode != null && destinationCode != null) {
+        var output = "a"
+        GlobalScope.launch(dispatchers.io) {
+
+            output = sequentialRequests() }
+        return output
+        /*return if (originCode != null && destinationCode != null) {
             "https://www.lner.co.uk/travel-information/travelling-now/live-train-times/depart/$originCode/$destinationCode/#LiveDepResults"
         } else {
             "https://www.lner.co.uk/travel-information/travelling-now/live-train-times/depart/EUS/BHM/#LiveDepResults"
-        }
+        }*/
     }
 
+    suspend fun sequentialRequests(): String {
+        val client = HttpClient() {
+            install(JsonFeature) {
+                serializer = KotlinxSerializer()
+            }
+        }
+        // Get the content of an URL.
+        val output: HttpResponse = client.get<HttpResponse>("https://mobile-api-dev.lner.co.uk/v1/fares?originStation=EUS&destinationStation=BHM&outboundDateTime=2020-07-15T12%3A16%3A27.371%2B00%3A00&inboundDateTime=2020-03-06T12%3A16%3A27.371%2B00%3A00&numberOfChildren=1&numberOfAdults=0&doSplitTicketing=false")
+        println("Hi")
+        println(output.readText())
+
+        return output.readText()
+    }
 
 
 }
