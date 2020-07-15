f547aff2235115b88013b2bc2a4f21e89df353d8
Thomas Haslam <thomas.haslam@softwire.com>
2020-07-15 12:21:24 +0100

    refactoring setup

--- app/src/main/java/com/jetbrains/handson/mpp/mobile/MainActivity.kt
+++ app/src/main/java/com/jetbrains/handson/mpp/mobile/MainActivity.kt
@@ -8,33 +8,44 @@ import android.os.Bundle
 import android.widget.*
 
 class MainActivity : AppCompatActivity(), ApplicationContract.View {
-    lateinit private var autoAdapter:ArrayAdapter<String>
+    lateinit private var autoAdapter: ArrayAdapter<String>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val presenter = ApplicationPresenter()
         presenter.onViewTaken(this)
-        autoAdapter=ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line, mutableListOf())
+        setupAutos()
+        setupButton(presenter)
+    }
+
+    private fun setupAutos() {
+        autoAdapter =
+            ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, mutableListOf())
         val autos = listOf<AutoCompleteTextView>(
             findViewById(R.id.arrival_station),
             findViewById(R.id.departure_station)
         )
-        autos.forEach{
+        autos.forEach {
+
             it.setAdapter(autoAdapter);
         }
+    }
+
+    private fun setupButton(presenter: ApplicationPresenter) {
         val button: Button = findViewById(R.id.done_button)
         button.setOnClickListener {
             presenter.onDoneButtonPressed()
         }
     }
 
+
     override fun openURL(url: String) {
         startActivity(Intent(ACTION_VIEW, Uri.parse(url)))
     }
 
     override fun getArrivalDepartureStations(): Pair<String, String> {
-        val arrivalText:AutoCompleteTextView = findViewById(R.id.arrival_station)
-        val departureText:AutoCompleteTextView = findViewById(R.id.departure_station)
+        val arrivalText: AutoCompleteTextView = findViewById(R.id.arrival_station)
+        val departureText: AutoCompleteTextView = findViewById(R.id.departure_station)
         return Pair(
             arrivalText.text.toString(),
             departureText.text.toString()
