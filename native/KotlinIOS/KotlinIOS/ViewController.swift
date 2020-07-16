f7590e35cd0d7c10faf9031efb15e434d911edbc
shavi-jay <60151734+shavi-jay@users.noreply.github.com>
2020-07-16 01:15:45 +0100

    Tidied up picker code and implemented toolbar

--- native/KotlinIOS/KotlinIOS/ViewController.swift
+++ native/KotlinIOS/KotlinIOS/ViewController.swift
@@ -12,62 +12,39 @@ class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelega
     
     var stations=["Edinburgh Waverly","King's Cross","York","Durham","Cambridge"]
     
-    
     //Creating new picker
     
     @IBOutlet private var departure_field: UITextField!
     
     @IBOutlet private var arrival_field: UITextField!
     
-    var departure_picker2 = UIPickerView()
-    var arrival_picker2 = UIPickerView()
-    
+    var departure_picker = UIPickerView()
+    var arrival_picker = UIPickerView()
+    var currentDeparture = ""
+    var currentArrival = ""
+    let toolBarDeparture = UIToolbar()
+    let toolBarArrival = UIToolbar()
+    let doneButtonDeparture = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(doneClickDeparture))
+    let doneButtonArrival = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(doneClickArrival))
+    let spaceButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+    let cancelButtonDeparture = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(cancelClick))
+    let cancelButtonArrival = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(cancelClick))
 
+    //Table variables
     
     private var tableContents:Array<String>=[]
     private let tableID="potato"
     private let presenter: ApplicationContractPresenter = ApplicationPresenter()
+    
+    
+    //On load
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         presenter.onViewTaken(view: self)
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: tableID)
-        //New Departure Picker
-        departure_field.inputView = departure_picker2
-        departure_picker2.delegate=self
-        departure_picker2.dataSource=self
         
-        //New Arrival Picker
-        arrival_field.inputView = arrival_picker2
-        arrival_picker2.delegate=self
-        arrival_picker2.dataSource=self
-    }
-    
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    
-    //Picker Section
-    
-    // The number of rows of data
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return stations.count
-    }
-    
-    // The data to return for the row and component (column) that's being passed in
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return stations[row]
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        
-        if pickerView == departure_picker2 {
-        departure_field.text = stations[departure_picker2.selectedRow(inComponent: 0)]
-            departure_field.resignFirstResponder()
-        } else {
-        arrival_field.text = stations[arrival_picker2.selectedRow(inComponent: 0)]
-        arrival_field.resignFirstResponder()
-        }
+        createpickers()
     }
     
     @IBAction func button_press(_ sender: Any) {
@@ -125,3 +102,75 @@ extension ViewController: UITableViewDataSource,UITableViewDelegate{
     }
     
 }
+
+
+//Picker Stuff
+
+extension ViewController {
+    func createpickers(){
+        //New Departure Picker
+        departure_field.inputView = departure_picker
+        departure_picker.delegate=self
+        departure_picker.dataSource=self
+        toolBarDeparture.barStyle = .default
+        toolBarDeparture.isTranslucent = true
+        toolBarDeparture.setItems([cancelButtonDeparture, spaceButton, doneButtonDeparture], animated: false)
+        toolBarDeparture.sizeToFit()
+        toolBarDeparture.isUserInteractionEnabled = true
+        departure_field.inputAccessoryView = toolBarDeparture
+        
+        //New Arrival Picker
+        arrival_field.inputView = arrival_picker
+        arrival_picker.delegate=self
+        arrival_picker.dataSource=self
+        toolBarArrival.barStyle = .default
+        toolBarArrival.isTranslucent = true
+        toolBarArrival.setItems([cancelButtonArrival, spaceButton, doneButtonArrival], animated: false)
+        toolBarArrival.sizeToFit()
+        toolBarArrival.isUserInteractionEnabled = true
+        arrival_field.inputAccessoryView = toolBarArrival
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // The number of rows of data
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return stations.count
+    }
+    
+    // The data to return for the row and component (column) that's being passed in
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return stations[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        switch pickerView {
+        case departure_picker:
+            currentDeparture = stations[departure_picker.selectedRow(inComponent: 0)]
+        case arrival_picker:
+            currentArrival = stations[arrival_picker.selectedRow(inComponent: 0)]
+        default:
+            departure_field.resignFirstResponder()
+            arrival_field.resignFirstResponder()
+        }
+    }
+    
+    @objc func cancelClick() {
+        departure_field.resignFirstResponder()
+        arrival_field.resignFirstResponder()
+    }
+    
+    @objc func doneClickDeparture() {
+        departure_field.text = currentDeparture
+        departure_field.resignFirstResponder()
+    }
+    
+    @objc func doneClickArrival() {
+        arrival_field.text = currentArrival
+        arrival_field.resignFirstResponder()
+    }
+}
+
