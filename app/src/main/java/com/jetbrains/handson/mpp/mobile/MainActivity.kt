4a8ab3424fc8f8d18cf7e80ee1e0f4d5b3cdef35
shavi-jay <60151734+shavi-jay@users.noreply.github.com>
2020-07-17 01:59:35 +0100

    Custom Cells are working! Minor changes to UI. Still need to make a table "title".

--- app/src/main/java/com/jetbrains/handson/mpp/mobile/MainActivity.kt
+++ app/src/main/java/com/jetbrains/handson/mpp/mobile/MainActivity.kt
@@ -61,16 +61,32 @@ class MainActivity : AppCompatActivity(), ApplicationContract.View {
     }
 
     override fun showData(data: List<ApplicationContract.TrainJourney>) {
-        println(data)
         val numberOfJourneys = data.size
-        var departureTimes = mutableListOf<String>()
+        val departureTimes = mutableListOf<String>()
+        val arrivalTimes = mutableListOf<String>()
+        val stationChanges = mutableListOf<String>()
+        val pricesList = mutableListOf<String>()
 
         for (i in 0 until (numberOfJourneys-1)){
-            departureTimes.add(data[i].departureTime + ", " + data[i].arrivalTime + ": £" + (data[i].cost/100).toString() + "." + ((data[i].cost%100).toString()))
+            departureTimes.add(data[i].departureTime)
+            arrivalTimes.add(data[i].arrivalTime)
+            val legs = 1 //TODO: Obtain Number of Legs in Journey
+            stationChanges.add((legs - 1).toString())
+            if (data[i].cost%100 == 0) {
+                pricesList.add("£" + (data[i].cost/100).toString() + ".00")
+            } else {
+                pricesList.add("£" + (data[i].cost/100).toString() + "." + (data[i].cost%100).toString())
+            }
+
         }
-        tableData = departureTimes
+        obtainedDepartureData = departureTimes
+        obtainedArrivalData = arrivalTimes
+        obtainedChangesData = stationChanges
+        obtainedPricesData = pricesList
         setupTable()
     }
+
+
 
     override fun updateStations(data: List<String>) {
         autoAdapter.clear()
@@ -83,13 +99,15 @@ class MainActivity : AppCompatActivity(), ApplicationContract.View {
 
     //Results Table
 
-    private var tableData = listOf<String>()
+    private var obtainedDepartureData = listOf<String>()
+    private var obtainedArrivalData = listOf<String>()
+    private var obtainedChangesData = listOf<String>()
+    private var obtainedPricesData = listOf<String>()
 
     private fun setupTable() {
         val layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
         val adapter = ResultsTableAdapter()
-        adapter.updateData(tableData)
-
+        adapter.updateData(obtainedDepartureData,obtainedArrivalData,obtainedChangesData,obtainedPricesData)
         resultsTable.apply {
             this.layoutManager = layoutManager
             this.adapter = adapter
@@ -101,11 +119,18 @@ class MainActivity : AppCompatActivity(), ApplicationContract.View {
 //Results Table
 
 class  ResultsTableAdapter: RecyclerView.Adapter<ResultsTableAdapter.MyViewHolder>() {
-    private var myData = emptyList<String>()
+    private var departureData = emptyList<String>()
+    private var arrivalData = emptyList<String>()
+    private var changesData = emptyList<String>()
+    private var pricesData = emptyList<String>()
+
 
     inner class MyViewHolder(view: View): RecyclerView.ViewHolder(view) {
-        fun bindData(text: String, onButtonClick:() -> Unit) {
-            itemView.resultsTableCellText1.text = text
+        fun bindData(departTime: String,arriveTime: String, trainChanges: String, price: String, onButtonClick:() -> Unit) {
+            itemView.resultsTableCellText1.text = departTime
+            itemView.resultsTableCellText2.text = arriveTime
+            itemView.resultsTableCellText3.text = trainChanges
+            itemView.resultsTableCellText4.text = price
             itemView.myButton.setOnClickListener {
                 onButtonClick()
             }
@@ -119,17 +144,22 @@ class  ResultsTableAdapter: RecyclerView.Adapter<ResultsTableAdapter.MyViewHolde
     }
 
     override fun getItemCount(): Int {
-        return myData.size
+        return departureData.size
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         holder.bindData(
-            myData[position]
-        ) { println(myData[position])}
+            departureData[position],arrivalData[position],changesData[position],pricesData[position]
+        ) {
+            //TODO: Url to website
+        }
     }
 
-    fun updateData(data: List<String>) {
-        myData = data
+    fun updateData(departData: List<String>,arriveData: List<String>,changeData: List<String>,priceData: List<String>) {
+        departureData = departData
+        arrivalData = arriveData
+        changesData = changeData
+        pricesData = priceData
         notifyDataSetChanged()
     }
 }
