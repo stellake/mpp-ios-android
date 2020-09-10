c5f8e375c881828ee1cad15495f597047de0cfe6
Cameron McCormack <Cameron.McCormack@softwire.com>
2020-09-10 09:07:14 +0100

    Commit of first day's work on android native code. Dropdown spinners added and spinner text colour and format changed.

--- app/src/main/java/com/jetbrains/handson/mpp/mobile/MainActivity.kt
+++ app/src/main/java/com/jetbrains/handson/mpp/mobile/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.jetbrains.handson.mpp.mobile
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity(), ApplicationContract.View {
 
@@ -12,6 +18,22 @@ class MainActivity : AppCompatActivity(), ApplicationContract.View {
 
         val presenter = ApplicationPresenter()
         presenter.onViewTaken(this)
+
+        // Find departure and arrival spinners from XML
+        val spinnerDep: Spinner = findViewById(R.id.departure_station)
+        val spinnerArr: Spinner = findViewById(R.id.arrival_station)
+
+        // Create an ArrayAdapter using the string array and the custom text formatting
+        val adapter: ArrayAdapter<String> = ArrayAdapter<String>(
+            applicationContext,
+            R.layout.spinner_item,
+            arrayOf("Station1", "Station2", "Station3", "Station4", "Station5")
+        )
+
+        // Set the drop down view to use a default spinner item on top of the custom text format
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinnerDep.adapter = adapter
+        spinnerArr.adapter = adapter
     }
 
     override fun setLabel(text: String) {
