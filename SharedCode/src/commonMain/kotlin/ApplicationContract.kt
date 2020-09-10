8dfbcfbe0cb2e233bc6b481142dd2452ef516a42
Will Page <billyoyo@hotmail.co.uk>
2020-09-10 09:27:30 +0100

    update application contract

--- SharedCode/src/commonMain/kotlin/ApplicationContract.kt
+++ SharedCode/src/commonMain/kotlin/ApplicationContract.kt
@@ -5,9 +5,19 @@ import kotlinx.coroutines.CoroutineScope
 interface ApplicationContract {
     interface View {
         fun setLabel(text: String)
+
+        fun setStations(stations: Array<Station>)
+
+        fun showAlert(message: String)
+
+        fun getDepartureStation(): Station
+
+        fun getArrivalStation(): Station
     }
 
     abstract class Presenter: CoroutineScope {
         abstract fun onViewTaken(view: View)
+
+        abstract fun onTimesRequested()
     }
 }
