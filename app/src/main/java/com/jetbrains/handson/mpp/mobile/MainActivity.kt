4933fa66096d0e3c91556d4bd88a2fa0bf40519e
Kabir Seth <kabset@dingo.zoo.lan>
2021-06-28 16:03:06 +0100

    early button logic

--- app/src/main/java/com/jetbrains/handson/mpp/mobile/MainActivity.kt
+++ app/src/main/java/com/jetbrains/handson/mpp/mobile/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.jetbrains.handson.mpp.mobile
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.TextView
+import androidx.core.content.ContentProviderCompat.requireContext
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), ApplicationContract.View {
+    lateinit var departureStationSelected: Spinner
+    lateinit var arrivalStationSelected: Spinner
+
+    lateinit var selectedDeparture: String
+    lateinit var selectedArrival: String
+
+    lateinit var departureCode: String
+    lateinit var arrivalCode: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,7 +29,54 @@ class MainActivity : AppCompatActivity(), ApplicationContract.View {
         val presenter = ApplicationPresenter()
         presenter.onViewTaken(this)
 
+        departureStationSelected = findViewById<Spinner>(R.id.station_names_spinner_departure) as Spinner
+        arrivalStationSelected = findViewById<Spinner>(R.id.station_names_spinner_arrival) as Spinner
 
+        if (departureStationSelected != null) {
+            val adapter = ArrayAdapter(
+                requireContext(),
+                android.R.layout.simple_spinner_item,
+                station_names_spinner_departure
+            )
+            departureStationSelected.adapter = adapter
+
+            departureStationSelected.onItemSelectedListener =
+                object : AdapterView.OnItemSelectedListener {
+                    override fun onItemSelected(
+                        parent: AdapterView<*>,
+                        view: View,
+                        position: Int,
+                        id: Long
+                    ) {
+                        departureCode =
+                            station_names_spinner_departure.split(" ")[-1].replace("[", "")
+                                .replace("]", "")
+                    }
+                }
+        }
+
+        if (arrivalStationSelected != null) {
+            val adapter = ArrayAdapter(
+                requireContext(),
+                android.R.layout.simple_spinner_item,
+                station_names_spinner_arrival
+            )
+            arrivalStationSelected.adapter = adapter
+
+            arrivalStationSelected.onItemSelectedListener =
+                object : AdapterView.OnItemSelectedListener {
+                    override fun onItemSelected(
+                        parent: AdapterView<*>,
+                        view: View,
+                        position: Int,
+                        id: Long
+                    ) {
+                        arrivalCode =
+                            station_names_spinner_departure.split(" ")[-1].replace("[", "")
+                                .replace("]", "")
+                    }
+                }
+        }
     }
 
     override fun setLabel(text: String) {
@@ -23,7 +84,16 @@ class MainActivity : AppCompatActivity(), ApplicationContract.View {
     }
 
     fun onSubmitButtonTapped(view: View) {
-        
+//        departureStationSelected = station_names_spinner_departure.selectedItem.toString()
+//        arrivalStationSelected  = station_names_spinner_arrival.selectedItem.toString()
+//
+//        departureCode = departureStationSelected.split(" ")[-1].replace("[", "").replace("]", "")
+//        arrivalCode = arrivalStationSelected.split(" ")[-1].replace("[", "").replace("]", "")
+
+        val url = "https://www.lner.co.uk/travel-information/travelling-now/live-train-times/depart/$departureCode/$arrivalCode/#LiveDepResults"
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse(url)
+        startActivity(intent)
     }
 
 
