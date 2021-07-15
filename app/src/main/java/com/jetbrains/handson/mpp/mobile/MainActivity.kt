abdaa184944ea717971401c0b782b1ba3129310d
Woody Lam <woody.lam@softwire.com>
2021-07-15 10:55:02 +0100

    Update Android UI

--- app/src/main/java/com/jetbrains/handson/mpp/mobile/MainActivity.kt
+++ app/src/main/java/com/jetbrains/handson/mpp/mobile/MainActivity.kt
@@ -4,8 +4,12 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.*
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 /**
  * The main view for the Android app
@@ -17,6 +21,7 @@ class MainActivity : AppCompatActivity(), ApplicationContract.View {
     private lateinit var fromSpinner: Spinner
     private lateinit var toSpinner: Spinner
     private lateinit var searchButton: Button
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,6 +54,7 @@ class MainActivity : AppCompatActivity(), ApplicationContract.View {
         val adapter = ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, stations)
         fromSpinner.adapter = adapter
         toSpinner.adapter = adapter
+        toSpinner.setSelection(1)
     }
 
     override fun openUrl(url: String) {
@@ -64,8 +70,31 @@ class MainActivity : AppCompatActivity(), ApplicationContract.View {
     }
 
     override fun displayJourneys(journeyCollection: JourneyCollection) {
-        setTitle(journeyCollection.outboundJourneys.first().destinationStation.displayName, "")
-        TODO("Display journeys in UI")
+        val adapter = RecyclerViewAdapter(journeyCollection)
+
+        recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 
+}
+
+
+class RecyclerViewAdapter(private val dataSet: JourneyCollection) : RecyclerView.Adapter<RecyclerViewAdapter.RecyclerViewHolder>() {
+
+    class RecyclerViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.resultText)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.result_item, parent, false)
+
+        return RecyclerViewHolder(view)
+    }
+
+    override fun onBindViewHolder(viewHolder: RecyclerViewHolder, position: Int) {
+        viewHolder.textView.text = dataSet.outboundJourneys[position].departureTime
+    }
+
+    override fun getItemCount() = dataSet.outboundJourneys.size
 }
