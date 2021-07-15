4192e6e67d168e20f833a0bfdee7593ad56e76b0
Louis Van Steene <Louis.VanSteene@softwire.com>
2021-07-15 15:02:54 +0100

    Use current time in API call

--- SharedCode/src/commonMain/kotlin/common.kt
+++ SharedCode/src/commonMain/kotlin/common.kt
@@ -1,17 +1,15 @@
 package com.jetbrains.handson.mpp.mobile
 
+import com.soywiz.klock.DateTime
+import com.soywiz.klock.TimeSpan
 import io.ktor.client.HttpClient
 import io.ktor.client.features.ClientRequestException
 import io.ktor.client.features.json.JsonFeature
 import io.ktor.client.features.json.serializer.KotlinxSerializer
-import io.ktor.client.request.HttpRequestBuilder
 import io.ktor.client.request.get
 import io.ktor.client.request.parameter
-import io.ktor.client.statement.readBytes
 import io.ktor.client.statement.readText
 import io.ktor.utils.io.core.use
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 import kotlinx.serialization.ImplicitReflectionSerializer
 import kotlinx.serialization.UnstableDefault
 import kotlinx.serialization.json.Json
@@ -40,6 +38,7 @@ fun createStations(): List<String> {
 @ImplicitReflectionSerializer
 @UnstableDefault
 suspend fun queryApi(from: String, to: String) : ApiResponse {
+    val currentTime = DateTime.now() + TimeSpan(60000.0)
     try {
         val response: JourneyCollection = HttpClient {
             install(JsonFeature) {
@@ -55,7 +54,7 @@ suspend fun queryApi(from: String, to: String) : ApiResponse {
                 parameter("numberOfAdults", 2)
                 parameter("numberOfChildren", 0)
                 parameter("journeyType", "single")
-                parameter("outboundDateTime", "2021-07-24T15:15:00.000+01:00")
+                parameter("outboundDateTime", currentTime.format("YYYY-MM-ddTHH:mm:ssXXX"))
                 parameter("outboundIsArriveBy", "false")
             }
         }
