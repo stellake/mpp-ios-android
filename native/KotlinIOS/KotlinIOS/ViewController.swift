180bddd5c8bc00d0c7bc135624355290f7035e14
Louis Van Steene <Louis.VanSteene@softwire.com>
2021-07-15 11:58:32 +0100

    Load data into table view

--- native/KotlinIOS/KotlinIOS/ViewController.swift
+++ native/KotlinIOS/KotlinIOS/ViewController.swift
@@ -7,16 +7,19 @@ class ViewController: UIViewController {
     @IBOutlet private var subHeader: UILabel!
     @IBOutlet private var picker: UIPickerView!
     @IBOutlet private var button: UIButton!
+    @IBOutlet private var resultsTable: UITableView!
     
     private var pickerData: [String] = [String]()
     private var fromSelected: String!
     private var toSelected: String!
+    private var journeyCollection: JourneyCollection!
     
     private let presenter: ApplicationContractPresenter = ApplicationPresenter()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         presenter.onViewTaken(view: self)
+        
     }
     
     @IBAction func sendSelection() {
@@ -26,11 +29,15 @@ class ViewController: UIViewController {
 
 extension ViewController: ApplicationContractView {
     func displayErrorMessage(message: String) {
-        
+        let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        self.present(alert, animated: true, completion: nil)
     }
     
     func displayJourneys(journeyCollection: JourneyCollection) {
-        
+        self.journeyCollection = journeyCollection
+        resultsTable.dataSource = self
+        resultsTable.reloadData()
     }
     
     func openUrl(url: String) {
@@ -50,6 +57,7 @@ extension ViewController: ApplicationContractView {
         toSelected = stations[1]
         picker.dataSource = self
         picker.delegate = self
+        picker.selectRow(1, inComponent: 1, animated: false)
     }
 }
 
@@ -59,7 +67,7 @@ extension ViewController: UIPickerViewDataSource {
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return 5
+        return pickerData.count
     }
 }
 
@@ -74,5 +82,18 @@ extension ViewController: UIPickerViewDelegate {
         case 1: toSelected = pickerData[row]
         default: break
         }
+    }
+}
+
+extension ViewController : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "results_item", for: indexPath)
+        
+        cell.textLabel!.text = journeyCollection.outboundJourneys[indexPath.item].departureTime
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return journeyCollection.outboundJourneys.count
     }
 }
